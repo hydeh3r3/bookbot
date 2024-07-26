b8bf2b7f374113bca206524e78ad53c246865323
hydeh3r3 <alituncgenc@gmail.com>
2024-07-27 02:29:19 +0300

    main.py new function

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 main()
-
-def count_words():
-    with open("books/frankenstein.txt") as f:
-        words = f.read().split()
-        return len(words)
-
-print(count_words())
-
-def count_characters():
-    char_counts = {}
-    with open("books/frankenstein.txt") as f:
-        for char in f.read().lower():
-            if char.isalpha():
-                char_counts[char] = char_counts.get(char, 0) + 1
-    return char_counts
-
-print(count_characters())
